0fc806da0522b58130edadfa8f219bb394ef7d8c
Michael Mayer <michael.mayer1@edu.fh-joanneum.at>
2025-02-28 11:33:01 +0100

    Projektdateien hinzufügen.

---
+++ SWE_01_Klasse_Objekt/Form1.Designer.cs
@@ -0,0 +1,45 @@
+﻿namespace SWE_01_Klasse_Objekt
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            SuspendLayout();
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(13F, 32F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1359, 788);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+        }
+
+        #endregion
+    }
+}
---
+++ SWE_01_Klasse_Objekt/Form1.cs
@@ -0,0 +1,10 @@
+namespace SWE_01_Klasse_Objekt
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
